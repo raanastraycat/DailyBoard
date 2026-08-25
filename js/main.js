a64ff101b17083bed13519e661fc9e7fab1f4540
Zuzy-Chan~ <raana.straycat@gmail.com>
2026-08-25 07:17:18 +0700

    Fix import paths in main.js to use relative paths

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-import { initTugas } from "tugas.js";
-import { initCatatan } from "note.js";
-import { initTema } from ".theme.js";
-import { initCuaca } from "weather.js";
-import { ambilKutipan } from "kutipan.js";
+import { initTugas } from "./tugas.js";
+import { initCatatan } from "./note.js";
+import { initTema } from "./theme.js";
+import { initCuaca } from "./weather.js";
+import { ambilKutipan } from "./kutipan.js";
 
 function mulaiAplikasi() {
   initTugas();
